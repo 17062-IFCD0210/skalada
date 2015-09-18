26a2b90c26331ddc7880a0eb76e13dcd97847fc9
ieltxuorue <ieltxuorue@hotmail.com>
2015-09-18 08:35:44 +0200

    lugares

---
+++ WebContent/backoffice/pages/lugares/index.jsp
@@ -0,0 +1,79 @@
+<%@page import="com.ipartek.formacion.skalada.bean.Zona"%>
+<%@page contentType="text/html"%> 
+<%@page pageEncoding="UTF-8"%> 
+
+<%@page import="com.ipartek.formacion.skalada.bean.Mensaje"%>
+<%@page import="com.ipartek.formacion.skalada.bean.Grado"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="com.ipartek.formacion.skalada.Constantes"%>
+<jsp:include page="../includes/head.jsp"></jsp:include>
+<jsp:include page="../includes/nav.jsp"></jsp:include>
+
+<div id="page-wrapper">
+
+    
+	<div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Lugares  </h1>
+        </div>        <!-- /.col-lg-12 -->
+    </div>    <!-- /.row -->
+    
+    <div class="row">
+    
+        <% 
+			Mensaje msg = (Mensaje)request.getAttribute("msg");	
+			if (msg != null){
+				out.print("<div class='alert "+msg.getTipo()+" alert-dismissible' role='alert'>");
+					out.print("<button type='button' class='close' data-dismiss='alert' aria-label='Close'>");
+						out.print("<span aria-hidden='true'>&times;</span>");
+					out.print("</button>");
+					out.print("<strong>"+ msg.getTexto() +"</strong>");
+				out.print("</div>");
+			} 
+		%>
+		
+		
+		<%
+			ArrayList<Zona> zonas = (ArrayList<Zona>)request.getAttribute("zonas");
+			Zona zona = null;		
+		%>
+		
+	</div> <!-- /.row -->
+
+    <div class="row">
+   		<%
+   			for (int i = 0 ; i < zonas.size() ; i++ ){
+   				zona = zonas.get(i);   		
+   		%>
+   		<div class="col-md-6">
+	        <div class="panel panel-primary">
+	            <div class="panel-heading">
+	                <div class="row">
+	                    <div class="text-center">
+	                        <i class="fa fa-picture-o fa-5x"></i>
+	                    </div>
+	                    <div>
+	                        <div class="text-right">26</div>
+	                        <div class="huge text-center"><%=zona.getNombre().toUpperCase() %></div>
+	                    </div>
+	                </div>
+	            </div>
+                <a href="#">
+                    <div class="panel-footer">
+                        <span class="pull-left">View Details</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                        <div class="clearfix"></div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <%
+   			}
+        %>
+        
+	    
+	</div>
+
+</div>
+        
+<jsp:include page="../includes/foot.jsp"></jsp:include>            
---
+++ src/java/com/ipartek/formacion/skalada/controladores/LugaresController.java
@@ -0,0 +1,107 @@
+package com.ipartek.formacion.skalada.controladores;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ipartek.formacion.skalada.Constantes;
+import com.ipartek.formacion.skalada.bean.Mensaje;
+import com.ipartek.formacion.skalada.bean.Zona;
+import com.ipartek.formacion.skalada.modelo.ModeloSector;
+import com.ipartek.formacion.skalada.modelo.ModeloZona;
+
+/**
+ * Servlet implementation class LugaresController
+ */
+public class LugaresController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private RequestDispatcher dispatcher = null;
+	private ModeloZona modeloZona = null;
+	private Zona zona = null;
+	private ModeloSector modeloSector = null;
+	
+	//parametros
+	private int pAccion = Constantes.ACCION_LISTAR;	
+	private int pID	= -1;		//ID no valido	
+	private String pNombre;
+	
+	private Mensaje msg;
+       
+    /**
+     * Este metodo se ejecuta solo la primera vez que se llama al servlet
+     * Se usa para crear el modelo
+     */
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    	super.init(config);
+    	modeloZona = new ModeloZona();  
+    	modeloSector = new ModeloSector();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//recoger parametros
+		getParameters(request,response);
+		
+		//realizar accion solicitada
+		switch (pAccion) {
+		case Constantes.ACCION_DETALLE:
+			detalle(request,response);
+			break;
+		default:
+			listar(request,response);
+			break;
+		}
+			
+		dispatcher.forward(request, response);
+	}
+		
+	private void getParameters(HttpServletRequest request, HttpServletResponse response) {		
+		try {
+			request.setCharacterEncoding("UTF-8");
+			pAccion = Integer.parseInt(request.getParameter("accion"));		
+			if(request.getParameter("id") != null && !"".equalsIgnoreCase(request.getParameter("id"))){
+				pID = Integer.parseInt(request.getParameter("id"));
+			}
+		} catch(Exception e){
+			e.printStackTrace();
+		}		
+	}
+	
+	private void listar(HttpServletRequest request, HttpServletResponse response) {
+		ArrayList<Object> zonas = modeloZona.getAll(); 
+		ArrayList<Integer> cant_sector = new ArrayList<Integer>();
+		for(int i = 0 ; i < cant_sector.size() ; i++ ){
+			zona = (Zona)zonas.get(i);
+			modeloSector.getAllByZona(zona.getId()y);
+		}		
+		request.setAttribute("zonas", zonas);
+		request.setAttribute("cant_sector", cant_sector);
+		dispatcher = request.getRequestDispatcher(Constantes.VIEW_BACK_LUGARES_INDEX);		
+	}
+	
+	private void detalle(HttpServletRequest request, HttpServletResponse response) {
+//		zona = (Zona)modeloZona.getById(pID);
+//		request.setAttribute("zona", zona);
+//		request.setAttribute("titulo", zona.getNombre().toUpperCase());
+//		request.setAttribute("metodo", "Modificar");
+		dispatcher = request.getRequestDispatcher(Constantes.VIEW_BACK_LUGARES_DETALLE);		
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
