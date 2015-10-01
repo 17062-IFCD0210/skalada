4bdeafd7c7acbaf534ba4fc342bd82512d6e8d23
Ander Ipartek <auraga@ipartek.com>
2015-10-01 09:24:11 +0200

    Usar DataSource para conexion a BBDD

--- src/java/com/ipartek/formacion/skalada/modelo/DataBaseHelper.java
+++ src/java/com/ipartek/formacion/skalada/modelo/DataBaseHelper.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 /**
  * Clase especializada en trabajar con la Base Datos
  * La usaran los DAOs para abrir y cerrar conexiones
@@ -28,11 +31,33 @@ public class DataBaseHelper {
 	 * @return {@code Connection} la conexion abierta
 	 * @throws Exception
 	 */
-	public static Connection getConnection() throws Exception{		
+	public static Connection getConnection() throws Exception{
+		
+		/* 
+		 *  Conexion usando DriverManager
+		 * 
+		 * 
 		if ( con == null ){
 			Class.forName(DRIVER);
 			con = DriverManager.getConnection ("jdbc:mysql://" + SERVER + "/" + DATA_BASE, USER, PASS);
 		}
+		return con;
+		*/
+		
+		/* Conexion usando DataSource y PoolConexiones */
+		
+		InitialContext ctx = new InitialContext();
+		DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/skaladaDB");
+		if ( ds == null ) {
+			throw new Exception("Data source no encontrado!");
+		}else{	
+			con = ds.getConnection();
+		}	
+		
+		if ( con == null ) {
+			throw new Exception("No se ha podido establecer conexion");
+		}	
+		
 		return con;
 	}
 	
