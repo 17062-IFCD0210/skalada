9aefd6e05a61137b5358f2e98beaa59fcf9788d1
aritzrodri <jaguar.wolf@yahoo.es>
2015-10-05 13:10:43 +0200

    corregir con el FindBugs fallos en el codigo

--- src/java/com/ipartek/formacion/skalada/bean/Grado.java
+++ src/java/com/ipartek/formacion/skalada/bean/Grado.java
@@ -4,21 +4,22 @@ import java.io.Serializable;
 
 /**
  * Grado de dificultd para las vias
+ * 
  * @author Curso
  *
  */
 public class Grado implements Serializable {
 	private static final long serialVersionUID = -7009856922950747575L;
-	
+
 	/**
 	 * Atributos
 	 */
 	private int id;
 	private String nombre;
 	private String descripcion;
-	
+
 	/**
-	 * 
+	 *
 	 * @param nombre
 	 */
 	public Grado(String nombre) {
@@ -26,55 +27,58 @@ public class Grado implements Serializable {
 		this.setNombre(nombre);
 		this.setId(-1);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return id
 	 */
 	public int getId() {
 		return this.id;
 	}
+
 	/**
-	 * 
+	 *
 	 * @param id
 	 */
-	public void setId(int id) {
+	public final void setId(int id) {
 		this.id = id;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return
 	 */
 	public String getNombre() {
 		return this.nombre;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param nombre
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	/**
-	 * 
+	 *
 	 * @return
 	 */
-	
+
 	/**
-	 * 
+	 *
 	 * @return descripcion
 	 */
 	public String getDescripcion() {
 		return this.descripcion;
 	}
+
 	/**
-	 * 
+	 *
 	 * @param descripcion
 	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
+
 }
