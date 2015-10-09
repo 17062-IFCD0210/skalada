297f02d2b3f01c454507fcf978ae7f7fdacf9606
ieltxuorue <ieltxuorue@hotmail.com>
2015-10-09 10:46:02 +0200

    movidos controladores al backoffice

--- src/java/com/ipartek/formacion/skalada/Constantes.java
+++ src/java/com/ipartek/formacion/skalada/Constantes.java
@@ -45,26 +45,26 @@ public final class Constantes {
 	public static final String VIEW_BACK_INDEX = "backoffice/pages/index_back.jsp";
 	public static final String VIEW_BACK_DASHBOARD = "pages/index_back.jsp";
 
-	public static final String VIEW_BACK_VIAS_INDEX = "backoffice/pages/vias/index.jsp";
-	public static final String VIEW_BACK_VIAS_FORM = "backoffice/pages/vias/form.jsp";
+	public static final String VIEW_BACK_VIAS_INDEX = "pages/vias/index.jsp";
+	public static final String VIEW_BACK_VIAS_FORM = "pages/vias/form.jsp";
 
-	public static final String VIEW_BACK_GRADOS_INDEX = "backoffice/pages/grados/index.jsp";
-	public static final String VIEW_BACK_GRADOS_FORM = "backoffice/pages/grados/form.jsp";
+	public static final String VIEW_BACK_GRADOS_INDEX = "pages/grados/index.jsp";
+	public static final String VIEW_BACK_GRADOS_FORM = "pages/grados/form.jsp";
 
-	public static final String VIEW_BACK_TIPO_ESCALADA_INDEX = "backoffice/pages/tipo-escalada/index.jsp";
-	public static final String VIEW_BACK_TIPO_ESCALADA_FORM = "backoffice/pages/tipo-escalada/form.jsp";
+	public static final String VIEW_BACK_TIPO_ESCALADA_INDEX = "pages/tipo-escalada/index.jsp";
+	public static final String VIEW_BACK_TIPO_ESCALADA_FORM = "pages/tipo-escalada/form.jsp";
 
-	public static final String VIEW_BACK_ZONAS_INDEX = "backoffice/pages/zonas/index.jsp";
-	public static final String VIEW_BACK_ZONAS_FORM = "backoffice/pages/zonas/form.jsp";
+	public static final String VIEW_BACK_ZONAS_INDEX = "pages/zonas/index.jsp";
+	public static final String VIEW_BACK_ZONAS_FORM = "pages/zonas/form.jsp";
 
-	public static final String VIEW_BACK_SECTORES_INDEX = "backoffice/pages/sectores/index.jsp";
-	public static final String VIEW_BACK_SECTORES_FORM = "backoffice/pages/sectores/form.jsp";
+	public static final String VIEW_BACK_SECTORES_INDEX = "pages/sectores/index.jsp";
+	public static final String VIEW_BACK_SECTORES_FORM = "pages/sectores/form.jsp";
 
-	public static final String VIEW_BACK_ROLES_INDEX = "backoffice/pages/roles/index.jsp";
-	public static final String VIEW_BACK_ROLES_FORM = "backoffice/pages/roles/form.jsp";
+	public static final String VIEW_BACK_ROLES_INDEX = "pages/roles/index.jsp";
+	public static final String VIEW_BACK_ROLES_FORM = "pages/roles/form.jsp";
 
-	public static final String VIEW_BACK_USUARIOS_INDEX = "backoffice/pages/usuarios/index.jsp";
-	public static final String VIEW_BACK_USUARIOS_FORM = "backoffice/pages/usuarios/form.jsp";
+	public static final String VIEW_BACK_USUARIOS_INDEX = "pages/usuarios/index.jsp";
+	public static final String VIEW_BACK_USUARIOS_FORM = "pages/usuarios/form.jsp";
 
 	
 
@@ -72,15 +72,15 @@ public final class Constantes {
 	public static final String CONTROLLER_LOGIN = ROOT_APP + "login";
 	public static final String CONTROLLER_SIGNUP = ROOT_APP + "signup";
 	public static final String CONTROLLER_LOGOUT = ROOT_APP + "logout";
-	public static final String CONTROLLER_VIAS = ROOT_APP + "vias";
+	public static final String CONTROLLER_VIAS = ROOT_BACK + "vias";
 	public static final String CONTROLLER_HOME = ROOT_APP + "home";
-	public static final String CONTROLLER_GRADOS = ROOT_APP + "grados";
-	public static final String CONTROLLER_TIPO_ESCALADA = ROOT_APP + "tipo-escalada";
-	public static final String CONTROLLER_ZONAS = ROOT_APP + "zonas";
+	public static final String CONTROLLER_GRADOS = ROOT_BACK + "grados";
+	public static final String CONTROLLER_TIPO_ESCALADA = ROOT_BACK + "tipo-escalada";
+	public static final String CONTROLLER_ZONAS = ROOT_BACK + "zonas";
 	public static final String CONTROLLER_ZONAS_JSON = ROOT_APP + "zonas-json";
-	public static final String CONTROLLER_SECTORES = ROOT_APP + "sectores";
-	public static final String CONTROLLER_ROLES = ROOT_APP + "roles";
-	public static final String CONTROLLER_USUARIOS = ROOT_APP + "usuarios";
+	public static final String CONTROLLER_SECTORES = ROOT_BACK + "sectores";
+	public static final String CONTROLLER_ROLES = ROOT_BACK + "roles";
+	public static final String CONTROLLER_USUARIOS = ROOT_BACK + "usuarios";
 
 	// acciones
 	public static final int ACCION_NUEVO = 0;
@@ -117,10 +117,8 @@ public final class Constantes {
 	public static final int USER_VALIDATE = 1;
 	public static final int USER_NO_VALIDATE = 0;
 	public static final String EMAIL_TEMPLATE_REGISTRO = "file/plantilla-mail.html";
-	public static final String URL_VALIDATE = SERVER
-			+ "/skalada/signup?action=1&id=";
-	public static final String URL_PASS_OLVIDADO = SERVER
-			+ "/skalada/signup?action=2&email=";
+	public static final String URL_VALIDATE = SERVER + "/skalada/signup?action=1&id=";
+	public static final String URL_PASS_OLVIDADO = SERVER + "/skalada/signup?action=2&email=";
 
 	public static final String VALIDACION = "validacion";
 	public static final String RECUPERACION = "recuperacion";
